18b62b8917c72fa3f99d0d14f2741eaf1e7d7f5f
mechlog <43264670+mechlog@users.noreply.github.com>
2018-10-21 11:05:09 +0900

    Fix comment Add myfunc

--- startup.m
+++ startup.m
@@ -1,34 +1,53 @@
-% プロットの色
+% % % % % % % % % % % % % % % % % % % % % % % % %
+% startup.m
+%
+% Description: https://mechlog.hateblo.jp/entry/2017/09/16/225342
+%
+% Environment: Matlab 2016b
+%
+% Author: twitter @mech_log
+% % % % % % % % % % % % % % % % % % % % % % % % %
+
+%% 自作ライブラリの追加
+% （自作関数などを，起動フォルダ下のmy_funcフォルダ内に置く）
+addpath(genpath('myfunc'));
+
+%% 色に関する設定
+% プロットの色を指定
+% (plot関数などで色を指定しない場合，順番に以下の色が使われる)
 co = ...
-    [  0,   0, 255;     % 青
-    255,   0,   0;      % 赤
+    [255,   0,   0;      % 赤
+    0,   0, 255;     % 青
     0, 128,   0;        % 緑
-    204,   8, 204;      % 紫
     222, 125,   0;      % 茶
-    0,  51, 153;        % 紺
-    64,  64,  64];      % 灰
+    204,   8, 204;      % 紫
+    255,  0, 255;        % マゼンタ
+    64,  64,  64];      % 灰色
 set(0,'defaultAxesColorOrder',co/255);
 
-
-% figureウインドウの色（白に設定）
+% Figureウインドウの色（白に設定）
 set(0, 'defaultFigureColor',[1 1 1]);
 
 % Figureをコピーしたときのプロットボックスの色（透明に設定）
+% （PowerPointなどにFigureをコピーした際に，プロットボックスが透ける設定）
 set(0, 'defaultAxesColor','none');
 
-% figure軸のフォントの設定
+%% フォントの設定
+% グラフ軸のフォントの設定
 set(0,'defaultAxesFontSize',11);
 set(0,'defaultAxesFontName','times new roman');
 set(0,'defaultAxesXcolor','k');
 set(0,'defaultAxesYcolor','k');
+set(0,'defaultAxesZcolor','k');
 
-% figure内テキストのフォントの設定
+% テキストのフォントの設定
 set(0,'defaultTextFontSize',11);
 set(0,'defaultTextFontName','times new roman');
 set(0,'defaultTextColor','k');
 
 % プロットの線幅
 set(0,'defaultLineLineWidth',1);
+
 % 軸の線幅
 set(0,'defaultAxesLineWidth', 1);
 
@@ -36,22 +55,29 @@ set(0,'defaultAxesLineWidth', 1);
 set(0,'defaultLegendFontSize',11);
 set(0,'defaultLegendFontName','times new roman');
 
-
+%% Figureのスタイルの設定
 % 常に grid on
 set(0,'DefaultAxesXGrid','on');
 set(0,'DefaultAxesYGrid','on');
+set(0,'DefaultAxesZGrid','on');
+
 % 常に box on
 set(0,'DefaultAxesBox','on');
+
 % グリッドラインの透明度（ 1 は不透明，0 は完全に透明）
 set(0,'DefaultAxesGridAlpha',1);
+
 % グリッドラインのラインスタイル
 set(0,'DefaultAxesGridLineStyle','--');
 
 % Figureの保存時に，ウィンドウでの表示サイズのまま保存する
 set(0,'DefaultFigurePaperPositionMode','auto');
+
 % ウィンドウのサイズ変更を有効化
 set(0,'DefaultFigureResize','on');
 
-
 % startup.m が動いているか確認するための表示
-disp('Ready')
+slCharacterEncoding('Shift-JIS')
+currentCharacterEncoding = slCharacterEncoding()
+
+clear;
